67f5f65c8ff7ce1af4403855373f8a5cda64d067
Jonathan Kruse <jonathanskruse@gmail.com>
2018-03-10 13:15:57 -0600

    rows work

--- client.js
+++ client.js
@@ -8,15 +8,7 @@ function readyNow(){
           $(this).parent().parent().remove();
       });
 
-
-
-
-
 let salArr = [];
-// $('.submit').on('click', submitClick);
-// $('.outputTable').on('click', '.remove1', removeButton);
-
-
 
   function submitClick(){
 
@@ -26,6 +18,7 @@ let salArr = [];
     let jobT = $('.jobTitle').val();
     let sal = $('.salary').val();
     let empInfo = [firstN, lastN, empId, jobT, sal];
+    console.log(empInfo);
     salArr.push(sal);
 let total = salArr.reduce(getSum);
 
@@ -39,27 +32,27 @@ console.log(total)
       return parseInt(a) + parseInt(b);
   }
 
-  $("#inputFields").append('<tr valign="top"><th scope="row"><label for="customFieldName"></label></th><td>' ,firstN, '&nbsp;<input type="text" class="code" id="customFieldValue" name="customFieldValue[]" value="" placeholder="Last Name" /> &nbsp;<input type="text" class="code" id="customFieldId" name="customFieldValue[]" value="" placeholder="Id Number" /> &nbsp;<input type="text" class="code" id="customFieldTitle" name="customFieldValue[]" value="" placeholder="Job Title" /> &nbsp;<input type="text" class="code" id="customFieldSalary" name="customFieldValue[]" value="" placeholder="Salary" /> &nbsp; <a href="javascript:void(0);" class="remCF">Remove</a></td></tr>');
+  $("#inputFields").append('<tr valign="top"><th scope="row"><label for="customFieldName"></label></th><td class="formInfo">&nbsp;' + firstN + '&nbsp;<td class="formInfo">' + lastN + '</td> &nbsp;<td class="formInfo">' + empId + '</td>  &nbsp;<td class="formInfo">' + jobT + '</td> &nbsp;<td class="formInfo">' + sal + '</td> &nbsp;<td class="formInfo"> <a href="javascript:void(0);" class="remCF">Remove</a></td></tr>');
 
 
 
-    $('.firstT').append(firstN,"<th class='formInfo1'></th>");
-    $('.lastT').append(lastN, "<th class='formInfo2'></th>");
-    $('.idT').append(empId, "<th class='formInfo3'></th>");
-    $('.titleT').append(jobT, "<th class='formInfo4'></th>");
-    $('.numberT').append(sal, "<th class='formInfo5'></th>");
-    $('.remove1').append("<button class='removeB'>Remove</button><br><br><br");
-     // console.log(data);
-    $('.totalField').empty();
-    $('.totalField').append(total, "<br class='formInfo5'>");
+    // $('.firstT').append(firstN,"<th class='formInfo1'></th>");
+    // $('.lastT').append(lastN, "<th class='formInfo2'></th>");
+    // $('.idT').append(empId, "<th class='formInfo3'></th>");
+    // $('.titleT').append(jobT, "<th class='formInfo4'></th>");
+    // $('.numberT').append(sal, "<th class='formInfo5'></th>");
+    // $('.remove1').append("<button class='removeB'>Remove</button><br><br><br");
+      // console.log(data);
+    // $('.totalField').empty();
+    // $('.totalField').append(total, "<br class='formInfo5'>");
 
 
   }
 
-  function removeButton(){
-    console.log('hi')
-    $().closest('tr').empty();
-
-  }
+  // function removeButton(){
+  //   console.log('hi')
+  //   $().closest('tr').empty();
+  //
+  // }
 
 }
