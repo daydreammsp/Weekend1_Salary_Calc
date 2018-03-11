04f262dcac35571f0d781096e18b13fc8689f4e9
Jonathan Kruse <jonathanskruse@gmail.com>
2018-03-11 11:36:08 -0500

    math works

--- client.js
+++ client.js
@@ -1,10 +1,16 @@
 $(document).ready(readyNow);
 
+
+let employees = [];
+let r = [];
+let m;
+let h = [];
+let salArr = [];
+
+
 function readyNow(){
   console.log('js ready');
-let total;
-let total1 = [];
-let salArr = [];
+
 
 class Employee{
   constructor(first,last,empNum,jobTi,salary){
@@ -23,20 +29,17 @@ class Employee{
 
 
       $("#inputFields").on('click','.remove1',function(){
+        h = [];
+        let t = 0;
           $(this).parent().parent().remove().removeData();
-          // $('.totalField').empty();
-          // $('.totalField').append(total2, "<br class='formInfo5'>");
+           $('.totalField').empty();
+           remove();
+           //$('.formInfoSal').removeData(employees)
 
           });
 
 
-//           $( '.formInfoSal' ).each(function( index ) {
-//                let x =  parseInt($( this ).text()) ;
-//
-//
-//                console.log(x);
-//
-// });
+
 
 // $('.totalField').append(total1, "<br class='formInfo5'>");
 
@@ -49,26 +52,19 @@ class Employee{
 
 
   function submitClick(){
-
+// let total = 0;
+// salArr =[];
+h = [];
+let t = 0;
     let firstN = $('.firstName').val();
     let lastN = $('.lastName').val();
     let empId = $('.idNumber').val();
     let jobT = $('.jobTitle').val();
     let sal = $('.salary').val();
     let empInfo = [firstN, lastN, empId, jobT, sal];
-    salArr.push(sal);
-    total = salArr.reduce(sum);
-
-  for(let x of salArr){
-    let y = x++;
-
-  }
-
-
-
-
   $("#inputFields").append('<tr valign="top"><th scope="row"><label for="customFieldName"></label></th><td class="formInfo">&nbsp;' + firstN + '&nbsp;<td class="formInfo">' + lastN + '</td> &nbsp;<td class="formInfo">' + empId + '</td>  &nbsp;<td class="formInfo">' + jobT + '</td> &nbsp;<td class="formInfoSal">' + sal + '</td> &nbsp;<td class="formInfo"> <a href="javascript:void(0);" class="remove1">Remove</a></td></tr>');
-
+  $('.totalField').empty();
+  remove()
 
 
     // $('.firstT').append(firstN,"<th class='formInfo1'></th>");
@@ -78,17 +74,66 @@ class Employee{
     // $('.numberT').append(sal, "<th class='formInfo5'></th>");
     // $('.remove1').append("<button class='removeB'>Remove</button><br><br><br");
       // console.log(data);
-    $('.totalField').empty();
-    $('.totalField').append(total, "<br class='formInfo5'>");
-    $('.firstName, .lastName, .idNumber, .jobTitle, .salary').append().val('');
+      // salArr.push(sal);
+      // total = salArr.reduce(sum);
+      //
+      // for(let x of salArr){
+      // let y = x++;
+      //
+      // }
 
+    // $('.totalField').append(total, "<br class='formInfo5'>");
+
+    $('.firstName, .lastName, .idNumber, .jobTitle, .salary').append().val('');
+newEmp ();
+function newEmp (){
+  employees.push( new Employee( firstN, lastN, empId, jobT, sal ));
+// totalSal();
+salArr.push(sal);
+total = salArr.reduce(sum);
+
+for(let x of salArr){
+let y = x++;
+
+}
+}
+
+
+
+
+  //let m = $('.formInfoSal').data(employees).data();
+
+  // let r = employees[0].salary
+
+
+
+}
+function totalSal(){
+  r =[];
+
+  for(let i of employees){
+
+    r.push(parseInt(employees[i].salary))
+
+ m = r.reduce(sum);
+
+    console.log(m)
 
   }
+}
   function sum(a, b) {
       return parseInt(a) + parseInt(b);
   }
-  function sum1(a, b) {
-      return parseInt(a) - parseInt(b);
-  }
+function remove(){
+  $( '.formInfoSal' ).each(function( index ) {
+
+        h.push( parseInt($( this ).text()) );
+         t = h.reduce(sum);
+
+       console.log(t);
+
+});
+$('.totalField').append(t, "<br class='formInfo5'>");
+}
 
 }
